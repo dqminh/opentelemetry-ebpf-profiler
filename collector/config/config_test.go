7af6189b2c97f793ac0834eccebfb5d26b298c0b
Ivan Babrou <github@ivan.computer>
2026-05-26 20:13:00 -0700

    Add a min age filter to skip ephemeral processes
    In our environment we have a few big long lived processes that are interesting
    to profile, and a bunch of short lived stuff that barely contributes to load.
    Things like crons and monitoring scripts often exit before they get a chance
    to load their mappings, so they burn CPU unnecessarily.
    
    Here we allow skipping them based on age process age. If a process survives
    long enough, it's probably worth profiling. If it exits quickly, probably not.
    
    This is an opt-in and it is disabled by default.

--- collector/config/config_test.go
+++ collector/config/config_test.go
@@ -22,6 +22,7 @@ func validConfig() *Config {
 		ProbabilisticInterval:  1 * time.Minute,
 		ProbabilisticThreshold: 100,
 		NoKernelVersionCheck:   true,
+		ErrorMode:              PropagateError,
 	}
 }
 
@@ -196,4 +197,12 @@ func TestValidateErrorMode(t *testing.T) {
 			require.Equal(t, tt.want, cfg.ErrorMode)
 		})
 	}
+}
+
+func TestValidateFilterMinProcessAge(t *testing.T) {
+	cfg := validConfig()
+	cfg.FilterMinProcessAge = -1 * time.Second
+
+	err := xconfmap.Validate(cfg)
+	require.Error(t, err)
 }
