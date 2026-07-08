30d47004d731350099439e1363355281f89b7194
Ivan Babrou <github@ivan.computer>
2026-07-08 15:00:07 -0700

    Report main executable build id in TraceMeta
    This is a good proxy for running version for the cases where multiple
    versions of the same software are running simultaneously, for example
    during graceful upgrades.

--- libpf/trace.go
+++ libpf/trace.go
@@ -130,24 +130,25 @@ type Trace struct {
 
 // EbpfTrace represents a stack trace from Ebpf code.
 type EbpfTrace struct {
-	EnvVars          map[String]String
-	ProcessName      String
-	ExecutablePath   String
-	ContainerID      String
-	CustomLabels     map[String]String
-	Comm             Comm
-	FrameData        []uint64
-	FrameDataBuf     [3072]uint64
-	Value            int64
-	KTime            int64
-	CpuID            uint32
-	TID              PID
-	PID              PID
-	NumFrames        uint16
-	NumKernelFrames  uint16
-	Origin           uint16
-	APMTraceID       APMTraceID
-	APMTransactionID APMTransactionID
+	EnvVars               map[String]String
+	ProcessName           String
+	ExecutableMappingFile FrameMappingFileData
+	ExecutablePath        String
+	ContainerID           String
+	CustomLabels          map[String]String
+	Comm                  Comm
+	FrameData             []uint64
+	FrameDataBuf          [3072]uint64
+	Value                 int64
+	KTime                 int64
+	CpuID                 uint32
+	TID                   PID
+	PID                   PID
+	NumFrames             uint16
+	NumKernelFrames       uint16
+	Origin                uint16
+	APMTraceID            APMTraceID
+	APMTransactionID      APMTransactionID
 }
 
 type EbpfFrame []uint64
